72d45080deebc214596aea74410e42a2c0b4fd1b
eunryeong1 <lyungeg@gmail.com>
2023-04-07 18:01:05 +0900

    RESTFUL

--- src/main/java/gak/backend/member/api/MemberController.java
+++ src/main/java/gak/backend/member/api/MemberController.java
@@ -1,12 +1,43 @@
 package gak.backend.member.api;
 
+import gak.backend.member.dao.MemberRepository;
+import gak.backend.member.model.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Validated
 public class MemberController {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @PostMapping("/api/signup")
+    public String add(@RequestBody Member member){
+        memberRepository.save(member);
+        return "update OK";
+    }
+    @PostMapping("/api/item/login")
+    @ResponseBody
+    public List<Member> login(@RequestBody Member member){
+        List<Member> user = memberRepository.findByName(member.getName());
+
+
+        return user;
+    }
+    @GetMapping("/api/item")
+    public List<Member> get(){
+        return memberRepository.findAll();
+    }
+
+    @GetMapping("/api/item/{id}")
+    public Optional<Member> getId(@PathVariable("id")Long id){
+        return memberRepository.findById(id);
+    }
 
 }
--- src/main/java/gak/backend/member/dao/MemberRepository.java
+++ src/main/java/gak/backend/member/dao/MemberRepository.java
@@ -3,5 +3,8 @@ package gak.backend.member.dao;
 import gak.backend.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
 }
