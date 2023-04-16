56dafa63b5364be9236dcc4cf20d7d60c3b96324
yunjjung <yjung203@naver.com>
2023-04-17 00:09:35 +0900

    [ADD]Basetime 설정 추가

---
+++ src/main/java/gak/backend/domain/model/BaseTime.java
@@ -0,0 +1,22 @@
+package gak.backend.domain.model;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass //Entity가 BaseTime을 상속 받을 때, 생성 시간, 수정 시간을 인식하게 함.
+@EntityListeners(AuditingEntityListener.class) //자동으로 값을 넣어줌.
+@Getter
+public class BaseTime {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
