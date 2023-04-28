99287cc182433c82ee631d8d7db09215914963f7
yunjjung <yjung203@naver.com>
2023-04-28 16:20:11 +0900

    [ADD]클론 후 에러 수정

--- src/main/java/gak/backend/domain/member/dao/MemberRepository.java
+++ src/main/java/gak/backend/domain/member/dao/MemberRepository.java
@@ -12,11 +12,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //비밀번호 찾을 떄, 이메일로 찾아야함.
     Optional<Member> findByIdOrEmail(Long id, String email);
-    boolean existById(Long id);
+    boolean existsById(Long id);
     //탈퇴했던 계정인지 확인하기 위해 이메일이 존재하는지 확인.
-    boolean existByEmail(String email);
+    boolean existsByEmail(String email);
     //탈퇴후, 재가입이라면 멤버가 여러명일 수 있음.
     //생성할 때, 먼저 확인해야되나? -> existById를 통해서 먼저 확인하고 들어오면 될듯.
-    List<Member> findByEmail(Long id, String email);
+//    List<Member> findByEmail(Long id, String email);
 //    List<Member> findByEmail(String email);
 }
