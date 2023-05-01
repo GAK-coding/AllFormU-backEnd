6e849680465342314c03f6c8511829ca7cd0acb9
yunjjung <yjung203@naver.com>
2023-05-01 19:17:02 +0900

    [ADD]Member 조회 로직 추가

--- src/main/java/gak/backend/domain/member/application/MemberService.java
+++ src/main/java/gak/backend/domain/member/application/MemberService.java
@@ -2,8 +2,10 @@ package gak.backend.domain.member.application;
 
 import gak.backend.domain.member.dao.MemberRepository;
 import gak.backend.domain.member.dto.MemberDTO;
+import gak.backend.domain.member.exception.NotFoundMemberByEmailException;
 import gak.backend.domain.member.model.Member;
 import gak.backend.domain.member.model.Status;
+import gak.backend.global.error.NotFoundByIdException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -42,7 +44,7 @@ public class MemberService {
         //이메일이 이미 존재하는 회원이라면,
         if (memberRepository.existsByEmail(memberSaveRequest.getEmail())) {
             //이미 존재하는 이메일이라면, 멤버 목록을 보고 휴면 계정인것을 찾음. 리스트인 이유는 탈퇴, 탈퇴, 휴면했을 경우도 있을 수 있기 때문.
-            List<Member> members = memberRepository.findByEmail(memberSaveRequest.getEmail());
+            List<Member> members = memberRepository.findMembersByEmail(memberSaveRequest.getEmail());
             for (Member member : members) {
                 if (member.getStatus() == Status.STATUS_DORMANT) {
                     MemberInfoDTO memberInfoDTO = member.toMemberInfoDTO();
@@ -69,6 +71,45 @@ public class MemberService {
         return null;
     }
 
+//================================================Read==========================================
 
+    //아이디로 멤버 객체 조회
+    public Member readMemberById(Long id){
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundByIdException::new);
+        return member;
+    }
+    //아이디로 멤버 infoDTO 조회
+    public MemberInfoDTO readMemberDTOById(Long id){
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundByIdException::new);
+        return member.toMemberInfoDTO();
+    }
+
+
+    //이메일로 멤버 조회
+    public Member readMemberByEmail(String email){
+        Member member = memberRepository.findByEmail(email).orElseThrow(NotFoundByIdException::new);
+        return member;
+    }
+    //이메일로 멤버 infoDTO 조회
+    public MemberInfoDTO readMemberDTOByEmail(String email){
+        Member member = memberRepository.findByEmail(email).orElseThrow(NotFoundMemberByEmailException::new);
+        return member.toMemberInfoDTO();
+    }
+
+    //이메일로 비밀번호 조회
+    public int findPasswordByEmail(String email){
+        Member member = memberRepository.findByEmail(email).orElseThrow(NotFoundMemberByEmailException::new);
+        return member.getPassword();
+    }
+
+
+
+
+    //=========================================Update==================================
+
+
+
+
+    //===========================================Delete======================================
 
 }
