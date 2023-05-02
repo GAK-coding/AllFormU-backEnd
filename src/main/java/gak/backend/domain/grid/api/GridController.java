d1fa8e242abeeb529f0b1f0d450d677bbc774b8c
yunjjung <yjung203@naver.com>
2023-05-02 19:07:03 +0900

    [ADD]Grid, Section 추가 셋팅

---
+++ src/main/java/gak/backend/domain/grid/api/GridController.java
@@ -0,0 +1,13 @@
+package gak.backend.domain.grid.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+public class GridController {
+}
---
+++ src/main/java/gak/backend/domain/section/dao/SectionRepository.java
@@ -0,0 +1,10 @@
+package gak.backend.domain.section.dao;
+
+
+import gak.backend.domain.section.model.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Long> {
+}
