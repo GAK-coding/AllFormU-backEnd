d3939b500a5be954e8974664264d1b7b3db9edc9
yunjjung <yjung203@naver.com>
2023-05-03 20:46:36 +0900

    [ADD]member 생성 컨트롤러

--- src/main/java/gak/backend/domain/member/api/MemberController.java
+++ src/main/java/gak/backend/domain/member/api/MemberController.java
@@ -1,21 +1,34 @@
 package gak.backend.domain.member.api;
 
+import gak.backend.domain.member.application.MemberService;
 import gak.backend.domain.member.dao.MemberRepository;
+import gak.backend.domain.member.dto.MemberDTO;
 import gak.backend.domain.member.model.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+import static gak.backend.domain.member.dto.MemberDTO.*;
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @Validated
 public class MemberController {
+    private final MemberService memberService;
+    @PostMapping(value="/register")
+    public ResponseEntity<MemberInfoDTO> createMember(@RequestBody @Validated MemberSaveRequest memberSaveRequest){
+        MemberInfoDTO memberInfoDTO = memberService.createMember(memberSaveRequest);
+        return new ResponseEntity<>(memberInfoDTO, HttpStatus.CREATED);
+    }
+
 
 //    @Autowired
 //    MemberRepository memberRepository;
