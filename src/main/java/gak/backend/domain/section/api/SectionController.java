76cc19bc77184b73110ac06d6b5a2cf8c6bb744a
yunjjung <yjung203@naver.com>
2023-05-05 14:09:48 +0900

    [REFACTOR]question 브랜치 섹션 오류 수정

--- src/main/java/gak/backend/domain/section/api/SectionController.java
+++ src/main/java/gak/backend/domain/section/api/SectionController.java
@@ -1,8 +1,5 @@
-<<<<<<<< HEAD:src/main/java/gak/backend/domain/section/api/SectionController.java
 package gak.backend.domain.section.api;
-========
-package gak.backend.domain.selection.api;
->>>>>>>> ff5041f600b0607319663a998eeab174ed6d5c6a:src/main/java/gak/backend/domain/selection/api/SelectionController.java
+
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Validated
 @RequiredArgsConstructor
-<<<<<<<< HEAD:src/main/java/gak/backend/domain/section/api/SectionController.java
 public class SectionController {
-========
-public class SelectionController {
->>>>>>>> ff5041f600b0607319663a998eeab174ed6d5c6a:src/main/java/gak/backend/domain/selection/api/SelectionController.java
 }
