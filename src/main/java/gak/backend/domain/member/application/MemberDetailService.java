5e44502bb87c7552b534fbee05c395ce3f1c4cb7
yunjjung <yjung203@naver.com>
2023-05-20 23:24:57 +0900

    [ADD]JWT & SPring Security 추가_2

---
+++ src/main/java/gak/backend/domain/member/application/MemberDetailService.java
@@ -0,0 +1,11 @@
+package gak.backend.domain.member.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MemberDetailService {
+}
