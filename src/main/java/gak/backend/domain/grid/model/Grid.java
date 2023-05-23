6d99a617b12ee4d3ecf6031ca4443e5b31b0b4b7
eunryeong1 <lyungeg@gmail.com>
2023-05-23 11:49:30 +0900

    [REFACTOR] Grid초기설정 수정

--- src/main/java/gak/backend/domain/grid/model/Grid.java
+++ src/main/java/gak/backend/domain/grid/model/Grid.java
@@ -12,36 +12,36 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Grid extends BaseTime {
-    @Id
-    @GeneratedValue
-    @Column(name = "Grid_id")
-    private Long id;
-
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "quesion_id")
-    private Question question;
-    private String colOption;
-    private String rowOption;
-    private String answer;
-
-
-
-    public void create(String answer,String colOption, String rowOption){
-        this.answer=answer;
-        this.colOption=colOption;
-        this.rowOption=rowOption;
-    }
-    public void update(String answer){
-        this.answer=answer;
-    }
-
-    public void setColOption(String colOption) {
-        this.colOption = colOption;
-    }
-
-    public void setRowOption(String rowOption) {this.rowOption = rowOption;}
-
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "Grid_id")
+//    private Long id;
+//
+//    @JsonIgnore
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "quesion_id")
+//    private Question question;
+//    private String colOption;
+//    private String rowOption;
+//    private String answer;
+//
+//
+//
+//    public void create(String answer,String colOption, String rowOption){
+//        this.answer=answer;
+//        this.colOption=colOption;
+//        this.rowOption=rowOption;
+//    }
+//    public void update(String answer){
+//        this.answer=answer;
+//    }
+//
+//    public void setColOption(String colOption) {
+//        this.colOption = colOption;
+//    }
+//
+//    public void setRowOption(String rowOption) {this.rowOption = rowOption;}
+//
 
 
 }
