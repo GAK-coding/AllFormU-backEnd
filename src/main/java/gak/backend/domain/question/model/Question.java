90674455c3517a05ba2b46c454663f86259f4b12
yunjjung <yjung203@naver.com>
2023-05-24 21:01:52 +0900

    [ADD]Question 준이 오빠 반영 안된 부분 추가

--- src/main/java/gak/backend/domain/question/model/Question.java
+++ src/main/java/gak/backend/domain/question/model/Question.java
@@ -3,8 +3,10 @@ package gak.backend.domain.question.model;
 import gak.backend.domain.description.model.Description;
 import gak.backend.domain.form.model.Form;
 import gak.backend.domain.model.BaseTime;
+import gak.backend.domain.question.dto.QuestionDTO;
 import gak.backend.domain.selection.model.Selection;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -40,4 +42,39 @@ public class Question extends BaseTime {
     @Enumerated(EnumType.STRING)
     @Column(name = "question_type")
     private Format type;
+
+    public void setContent(String content){
+        this.content=content;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public void OptionsSetting(List<Selection> options) {
+        this.options = options;
+    }
+    public void DescriptionsSetting(List<Description> descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public void UpdateSelectQuestion(QuestionDTO questionDTO){
+        this.title=questionDTO.getTitle();
+        this.required=questionDTO.isRequired();
+        this.sectionNum=questionDTO.getSectionNum();
+        this.type=questionDTO.getType();
+
+    }
+    public void setForm(Form form) {
+        this.form = form;
+    }
+    public void setType(Format type) {
+        this.type = type;
+    }
+
+    public void setSectionNum(int sectionNum) {
+        this.sectionNum = sectionNum;
+    }
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
 }
