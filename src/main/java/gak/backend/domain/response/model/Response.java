5f8dabf0d0614bd23440a1ddd5f2d0ba5f5d83e8
yunjjung <yjung203@naver.com>
2023-05-24 20:43:47 +0900

    [ADD]ResponseController domain 추가

---
+++ src/main/java/gak/backend/domain/response/model/Response.java
@@ -0,0 +1,70 @@
+package gak.backend.domain.response.model;
+
+import gak.backend.domain.member.model.Member;
+import gak.backend.domain.model.BaseTime;
+import gak.backend.domain.question.model.Question;
+import gak.backend.domain.response.dto.ResponseDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Type;
+
+import static gak.backend.domain.response.dto.ResponseDTO.*;
+
+@Entity
+@Getter
+@Slf4j
+//@AllArgsConstructor
+@NoArgsConstructor
+public class Response extends BaseTime {
+    @Id
+    @GeneratedValue
+    @Column(name="response_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member responsor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="question_id")
+    private Question question;
+
+    private int num;
+
+    @Builder
+    public Response(Long id, Member responsor, Question question, int num ){
+        this.id = id;
+        this.responsor = responsor;
+        this.question = question;
+        this.num = num;
+    }
+
+    public ResponseInfoDTO toResponseInfoDTO(){
+        return ResponseInfoDTO.builder()
+                .id(this.id)
+                .responsorId(this.getResponsor().getId())
+                .questionId(this.getQuestion().getId())
+                .num(this.num)
+                .createdTime(super.getCreatedDate())
+                .modifiedTime(super.getModifiedDate())
+                .build();
+    }
+
+    public ResponseSimpleInfoDTO responseSimpleInfoDTO(Member responsor, Question question){
+        return ResponseSimpleInfoDTO.builder()
+                .responsorId(responsor.getId())
+                .questionId(question.getId())
+                .num(this.num)
+                .build();
+    }
+
+    public void updateResponse(int changeNum){
+        this.num = changeNum;
+    }
+
+}
