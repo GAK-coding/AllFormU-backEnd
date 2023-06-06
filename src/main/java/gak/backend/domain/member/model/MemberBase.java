550b2a249f7f2ee320b974fb48a155755d0641be
yunjjung <yjung203@naver.com>
2023-06-06 21:38:52 +0900

    [ADD]MemberBase 분리

--- src/main/java/gak/backend/domain/member/model/MemberBase.java
+++ src/main/java/gak/backend/domain/member/model/MemberBase.java
@@ -4,10 +4,7 @@ import gak.backend.domain.form.model.Form;
 import gak.backend.domain.member.dto.MemberDTO;
 import gak.backend.domain.model.BaseTime;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
 public class MemberBase extends BaseTime {
     @Id
@@ -32,12 +30,12 @@ public class MemberBase extends BaseTime {
     private String password;
     private String image;
 
-    @OneToMany(mappedBy = "author") //변경 가능성 있음.
-    private List<Form> responseForm = new ArrayList<>();
-
-
-    @OneToMany(mappedBy = "author")
-    private List<Form> requestForm = new ArrayList<>();
+//    @OneToMany(mappedBy = "author") //변경 가능성 있음.
+//    private List<Form> responseForm = new ArrayList<>();
+//
+//
+//    @OneToMany(mappedBy = "author")
+//    private List<Form> requestForm = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     @Column(name = "role_type")
@@ -49,5 +47,13 @@ public class MemberBase extends BaseTime {
 
     @Enumerated(EnumType.STRING)
     private LoginType loginType;
+
+
+    public void UpdateMemberNickname(String newNickname){ this.nickname = newNickname; }
+    public void UpdateMemberImage(String newImage){ this.image = newImage; }
+
+    public void UpdateMemberPassword(String newPwd){ this.password = newPwd; }
+    public void UpdateMemberStatus(Status newStatus){ this.status = newStatus;}
+    public void UpdateMemberRole(Role newRole){this.role = newRole;}
 
 }
