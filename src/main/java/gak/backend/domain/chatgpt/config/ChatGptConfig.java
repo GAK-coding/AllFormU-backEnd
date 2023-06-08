47f23dad20368cfab22c0e78294c085493533a1f
june <junnim123@naver.com>
2023-06-08 14:01:42 +0900

    chatgpt api키 교체

--- src/main/java/gak/backend/domain/chatgpt/config/ChatGptConfig.java
+++ src/main/java/gak/backend/domain/chatgpt/config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package gak.backend.domain.chatgpt.config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION="Authorization";
     public static final String BEARER="Bearer ";
-    public static final String API_KEY="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";
+    public static final String API_KEY="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";
     public static final String MODEL="text-davinci-003";
     //"gpt-3.5-turbo"
     public static final Integer MAX_TOKEN=3000; //300
